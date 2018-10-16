3eee017921c28b4a1882a27012d7c393d2fa9769
Miguel Abreu <miguel_aj91@hotmail.com>
2018-10-15 23:00:34 -0400

    Initial project state

---
+++ ITunesBookSearch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ITunesBookSearch
+//
+//  Created by MetaTedi on 10/15/18.
+//  Copyright © 2018 Tedev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
